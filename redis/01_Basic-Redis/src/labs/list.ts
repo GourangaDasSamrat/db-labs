bf491b86fab3ffadf7eb71c6939eff8e95e46ef1
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-09 17:33:10 +0600

    feat(redis): add lrange example
    - Added lrange command example
    - Demonstrates reading list elements

--- redis/01_Basic-Redis/src/labs/list.ts
+++ redis/01_Basic-Redis/src/labs/list.ts
@@ -2,8 +2,8 @@ import redisClient from "@/client";
 
 const init = async () => {
   // insert from left side
-  //   const lPush = await redisClient.lpush("messages", "I miss you");
-  //   console.log("LPush result =>", lPush);
+  //  const lPush = await redisClient.lpush("messages", "I love you");
+  //  console.log("LPush result =>", lPush);
 
   // insert from right side
   // const rPush = await redisClient.rpush("messages", "How are you");
@@ -24,6 +24,10 @@ const init = async () => {
   // blocking left pop -> its like lpop, its remove key from left side but difference between lop and blpop is if there is no element lop return null (in shell nil), but blop wait the time we set timeout, in that time if any value add its remove that, if not its also return null
   // const bLPop = await redisClient.blpop("messages", 10); // timeout in seconds
   // console.log("BLPop result =>", bLPop);
+
+  // read list -> first number is where to start and 2nd is where to stop, here 0 means start from first and -1 means to last of the list, its return an array
+  // const lRange = await redisClient.lrange("messages", 0, -1);
+  // console.log("LRange result =>", lRange);
 };
 
 init();
