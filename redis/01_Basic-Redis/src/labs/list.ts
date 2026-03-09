50714d93af7db102f3ff9ef6bdd80e66f5879a57
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-09 17:19:27 +0600

    feat(redis): add basic list lab
    - Implemented LPUSH, RPUSH, LPOP, RPOP
    - Added LLEN and BLPOP examples
    - Demonstrates Redis list operations

---
+++ redis/01_Basic-Redis/src/labs/list.ts
@@ -0,0 +1,29 @@
+import redisClient from "@/client";
+
+const init = async () => {
+  // insert from left side
+  //   const lPush = await redisClient.lpush("messages", "I miss you");
+  //   console.log("LPush result =>", lPush);
+
+  // insert from right side
+  // const rPush = await redisClient.rpush("messages", "How are you");
+  // console.log("RPush result =>", rPush);
+
+  // remove from left side
+  // const lPop = await redisClient.lpop("messages");
+  // console.log("LPop result =>", lPop);
+
+  // remove from left side
+  // const rPop = await redisClient.rpop("messages");
+  // console.log("LPop result =>", rPop);
+
+  // list length
+  // const lLen = await redisClient.llen("messages");
+  // console.log("LLen result =>", lLen);
+
+  // blocking left pop -> its like lpop, its remove key from left side but difference between lop and blpop is if there is no element lop return null (in shell nil), but blop wait the time we set timeout, in that time if any value add its remove that, if not its also return null
+  // const bLPop = await redisClient.blpop("messages", 10); // timeout in seconds
+  // console.log("BLPop result =>", bLPop);
+};
+
+init();
