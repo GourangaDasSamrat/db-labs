2ba1512a5cee114de777bd74c2beab8d2afbf307
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-10 16:19:39 +0600

    fix(db): Add constraints to player table
    - Added NOT NULL constraints to name, rank, and nationality
    - Added DEFAULT 0 for point
    - Added CHECK constraints for age and rank
    - Added UNIQUE constraints for name and rank

--- postgresql/01_crud-operations/create-tables.sql
+++ postgresql/01_crud-operations/create-tables.sql
@@ -1,9 +1,14 @@
 CREATE TABLE
     IF NOT EXISTS players (
         id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        name VARCHAR(100),
-        RANK INT,
-        point INT,
-        age INT,
-        nationality VARCHAR(100)
+        name VARCHAR(100) NOT NULL,
+        RANK INT NOT NULL,
+        point INT NOT NULL DEFAULT 0,
+        age INT CHECK (age > 0),
+        nationality VARCHAR(100) NOT NULL,
+
+        CONSTRAINT unique_player_name UNIQUE (name),
+        CONSTRAINT unique_rank UNIQUE (RANK),
+        CONSTRAINT positive_rank CHECK (RANK > 0),
+        CONSTRAINT positive_point CHECK (point >= 0)
     );
