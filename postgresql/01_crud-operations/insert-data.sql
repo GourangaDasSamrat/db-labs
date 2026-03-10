e40dacca0a23a6e2964b7971d3cd238e42a9fed3
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-10 15:46:03 +0600

    fix(postgresql): Update insert-data.sql schema
    - Changed column 'id' to 'point'
    - Added point values for players

--- postgresql/01_crud-operations/insert-data.sql
+++ postgresql/01_crud-operations/insert-data.sql
@@ -1,13 +1,13 @@
 INSERT INTO
-    players (id, name, RANK, age, nationality)
+    players (name, RANK, point, age, nationality)
 VALUES
-    (101, 'Aryna Sabalenka', 1, 27, 'Belarus'),
-    (102, 'Iga Swiatek', 2, 24, 'Poland'),
-    (103, 'Elena Rybakina', 3, 26, 'Kazakhstan'),
-    (104, 'Coco Gauff', 4, 21, 'USA'),
-    (105, 'Jessica Pegula', 5, 32, 'USA'),
-    (106, 'Amanda Anisimova', 6, 24, 'USA'),
-    (107, 'Jasmine Paolini', 7, 30, 'Italy'),
-    (108, 'Mirra Andreeva', 8, 18, 'Russia'),
-    (109, 'Elina Svitolina', 9, 31, 'Ukraine'),
-    (110, 'Victoria Mboko', 10, 19, 'Canada');
+    ('Aryna Sabalenka', 1, 10675, 27, 'Belarus'),
+    ('Iga Swiatek', 2, 7588, 24, 'Poland'),
+    ('Elena Rybakina', 3, 7253, 26, 'Kazakhstan'),
+    ('Coco Gauff', 4, 6803, 21, 'USA'),
+    ('Jessica Pegula', 5, 6583, 32, 'USA'),
+    ('Amanda Anisimova', 6, 6070, 24, 'USA'),
+    ('Jasmine Paolini', 7, 4232, 30, 'Italy'),
+    ('Mirra Andreeva', 8, 4001, 18, 'Russia'),
+    ('Elina Svitolina', 9, 3845, 31, 'Ukraine'),
+    ('Victoria Mboko', 10, 3211, 19, 'Canada');
