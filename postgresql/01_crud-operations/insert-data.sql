3b98ac843c7c9bb0294e5278b66b4f60be4b8b81
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-10 15:35:38 +0600

    feat(postgresql): add player data for CRUD operations
    - Added SQL script for inserting player data
    - Includes 10 player records with ID, name, rank, age, nationality
    - File: postgresql/01_crud-operations/insert-data.sql

---
+++ postgresql/01_crud-operations/insert-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    players (id, name, RANK, age, nationality)
+VALUES
+    (101, 'Aryna Sabalenka', 1, 27, 'Belarus'),
+    (102, 'Iga Swiatek', 2, 24, 'Poland'),
+    (103, 'Elena Rybakina', 3, 26, 'Kazakhstan'),
+    (104, 'Coco Gauff', 4, 21, 'USA'),
+    (105, 'Jessica Pegula', 5, 32, 'USA'),
+    (106, 'Amanda Anisimova', 6, 24, 'USA'),
+    (107, 'Jasmine Paolini', 7, 30, 'Italy'),
+    (108, 'Mirra Andreeva', 8, 18, 'Russia'),
+    (109, 'Elina Svitolina', 9, 31, 'Ukraine'),
+    (110, 'Victoria Mboko', 10, 19, 'Canada');
