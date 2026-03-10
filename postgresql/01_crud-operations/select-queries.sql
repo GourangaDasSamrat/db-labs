b0f24adb9a75abb9468aaa54363bca1fc3e62aff
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-10 15:53:27 +0600

    feat(postgresql): add select queries example
    - Added file for SQL select queries
    - Includes examples for reading all and specific columns

---
+++ postgresql/01_crud-operations/select-queries.sql
@@ -0,0 +1,18 @@
+-- Read all columns
+SELECT
+    *
+FROM
+    players
+
+-- Read specific column
+-- SELECT
+--     name
+-- FROM
+--     players
+
+-- Read specific columns
+-- SELECT
+--     name,
+--     RANK
+-- FROM
+--     players
