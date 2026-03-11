34a96f46aba958529ccde341f0f8b0cf93565c48
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-11 12:13:27 +0600

    feat(redis): add basic stream example
    - Implemented xadd command example
    - Added temperature stream data

---
+++ redis/01_Basic-Redis/src/labs/stream.ts
@@ -0,0 +1,20 @@
+import redisClient from "@/client";
+
+const init = async () => {
+  // streams add
+  /*
+  const xAdd = await redisClient.xadd(
+    "temperatures:us-ny:10007",
+    "*",
+    "temp_f",
+    "81.9",
+    "pressure",
+    "25.91",
+    "humidity",
+    "43",
+  );
+  console.log("xAdd result =>", xAdd);
+*/
+};
+
+init();
