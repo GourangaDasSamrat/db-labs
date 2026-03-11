5f429415a1a3374a256b0515ec25e982b068bfa4
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-11 14:21:42 +0600

    feat(sql): add IN and NOT IN operator examples
    - Added SQL query for IN operator
    - Added SQL query for NOT IN operator

---
+++ postgresql/02_operators/02_in-operator.sql
@@ -0,0 +1,15 @@
+-- in operator
+SELECT
+    *
+FROM
+    goats
+WHERE
+    sport IN ('Cricket', 'Soccer');
+
+-- not in operator
+SELECT
+    *
+FROM
+    goats
+WHERE
+    sport IN ('Cricket', 'Soccer');
