9ecc79a22771b3b1210ee304c80db99223c30f65
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-26 12:41:02 +0600

    feat(mysql): add insert data for players table
    - Added SQL script for initial player data
    - Populates name, rank, points, age, nationality

---
+++ mysql/01_crud-operations/insert-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    players (name, `RANK`, point, age, nationality)
+VALUES
+    ('Aryna Sabalenka', 1, 10675, 27, 'Belarus'),
+    ('Iga Swiatek', 2, 7588, 24, 'Poland'),
+    ('Elena Rybakina', 3, 7253, 26, 'Kazakhstan'),
+    ('Coco Gauff', 4, 6803, 21, 'USA'),
+    ('Jessica Pegula', 5, 6583, 32, 'USA'),
+    ('Amanda Anisimova', 6, 6070, 24, 'USA'),
+    ('Jasmine Paolini', 7, 4232, 30, 'Italy'),
+    ('Mirra Andreeva', 8, 4001, 18, 'Russia'),
+    ('Elina Svitolina', 9, 3845, 31, 'Ukraine'),
+    ('Victoria Mboko', 10, 3211, 19, 'Canada');
