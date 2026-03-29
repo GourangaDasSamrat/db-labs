3fd6677158c9325c238d1f5de758fb6623d3e266
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-29 16:57:23 +0600

    refactor(postgresql): improve goat_age_category function
    - Replaced IF/ELSIF with CASE statement
    - Added LANGUAGE, IMMUTABLE, RETURNS NULL ON NULL INPUT
    - Added alias for returned column

--- postgresql/11_stored-routine/user-defined-func.sql
+++ postgresql/11_stored-routine/user-defined-func.sql
@@ -1,20 +1,18 @@
 -- create or replace function
 CREATE
-OR REPLACE FUNCTION goat_age_category (p_age INT) RETURNS TEXT AS $$
+OR REPLACE FUNCTION goat_age_category (p_age INT) RETURNS TEXT LANGUAGE plpgsql IMMUTABLE RETURNS NULL ON NULL INPUT AS $$
 BEGIN
-    IF p_age < 25 THEN
-        RETURN 'Young';
-    ELSIF p_age BETWEEN 25 AND 35 THEN
-        RETURN 'Prime';
-    ELSE
-        RETURN 'Veteran';
-    END IF;
+    RETURN CASE
+        WHEN p_age < 25 THEN 'Young'
+        WHEN p_age <= 35 THEN 'Prime'
+        ELSE 'Veteran'
+    END;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- use that func
 SELECT
     NAME,
-    goat_age_category (age)
+    goat_age_category (age) AS age_category
 FROM
     goats;
