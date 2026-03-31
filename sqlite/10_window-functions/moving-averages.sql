3352ca3c0932ec4c8c6b864961ca9628a211d711
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-31 12:49:32 +0600

    feat(sqlite): add moving averages example
    - Implemented SQL query for moving averages
    - Utilizes window functions for calculation
    - Demonstrates running average on net worth

---
+++ sqlite/10_window-functions/moving-averages.sql
@@ -0,0 +1,12 @@
+-- moving averages
+SELECT
+    nickname,
+    career_titles,
+    biography,
+    est_net_worth_usd,
+    AVG(est_net_worth_usd) OVER (
+        ORDER BY
+            est_net_worth_usd
+    ) AS running_avg
+FROM
+    goat_profiles;
